8d1652150f660fac0d295bec00b7a3c66cf68ea7
Eunjin Han <108893986+ARS-CSGCL-EH@users.noreply.github.com>
2026-05-27 15:11:13 -0400

    updated GUI

--- FSP_Wheat/FSP_NTWH_SIM/2001_217_NTWH/grid1.bat
+++
@@ -1,6 +0,0 @@
-"E:\GitHub\WheatSim_GUI\CreateSoils\CreateSoilFiles.exe" "E:\GitHub\WheatSim_GUI\FSP_Wheat\FSP_NTWH_SIM\2001_217_NTWH\2001_217_NTWH.lyr" /GN 2001_217_NTWH /SN 217
-del output
-del element_elm
-del grid_bnd
-del datagen2.dat
-Dir  *.*  >dir.txt
